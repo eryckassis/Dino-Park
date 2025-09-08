02abde1d810c849c76cfe5d8c04a457fa57bc4a2
Eryck Assis <eng.assis.dev@gmail.com>
2025-09-08 18:21:35 -0300

    Add dinosaur "Baiano" to enclosures and update listing logs

--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ const recinto3 = parque.criarRecinto(3, "Herbivoro", "Casa Blanca", 3);
 
 const dino1 = parque.criarDinossauro("Rex", "Tyranossaurus", 5);
 const dino2 = parque.criarDinossauro("Blue", "Velociraptor", 3);
+const dino3 = parque.criarDinossauro("Baiano", "Baianosauro", 10);
 
 //Inserir dinossauro nos recintos.
 
 parque.inserirDinossauroNoRecinto("Rex", 2);
 parque.inserirDinossauroNoRecinto("Blue", 2);
+parque.inserirDinossauroNoRecinto("Baiano", 3);
 
 //Listar dinossauro do recinto 2.
-console.log("Dinossauros no recinto 2:", parque.listarDinossaurosDoRecinto(2));
+console.log("Dinossauros no recinto 3:", parque.listarDinossaurosDoRecinto(3));
+
 parque.transferirDinossauro("Blue", 1);
+
 console.log("Dinossauros no recinto 1:", parque.listarDinossaurosDoRecinto(1));
+
 console.log("Dinossauros no recinto 2:", parque.listarDinossaurosDoRecinto(2));
+
 console.log(
   "Todos os Dinossauros do parque :",
-  parque.listarDinossaurosDoRecinto()
+  parque.listarTodosDinossauros()
 );
-//
